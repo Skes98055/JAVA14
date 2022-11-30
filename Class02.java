4bb4e16902f9ac661760de6ef68df4f5b1bc1c4c
Skes98055 <91359104+Skes98055@users.noreply.github.com>
2022-11-30 10:11:34 +0800

    Create Class02.java

---
+++ Class02.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Class02 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a= sc.nextLine();
+        String b=a.toUpperCase();
+        System.out.println(b);
+    }
+}
