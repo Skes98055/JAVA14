df2b75f9e33206cb36b328158f84042ea3e7693b
Skes98055 <91359104+Skes98055@users.noreply.github.com>
2022-11-30 11:04:53 +0800

    Update Class02.java

--- Class02.java
+++ Class02.java
@@ -2,8 +2,9 @@ import java.util.*;
 public class Class02 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.print("輸入字串:");
         String a= sc.nextLine();
         String b=a.toUpperCase();
-        System.out.println(b);
+        System.out.println("轉成大寫:"+b);
     }
 }
